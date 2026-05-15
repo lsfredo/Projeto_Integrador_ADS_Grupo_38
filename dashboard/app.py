73cb010e72b0c85daf41c0c5e7e27254c9ca0bf3
Luis Felipe <luisfelipe.holanda27@gmail.com>
2026-05-15 10:55:51 -0300

    Correção do bug na métrica de conversão e adição do gráfico de distribuição de assinaturas
    - Corrigido o cálculo da métrica de conversão de anúncios, ajustando o tratamento dos dados para garantir a precisão dos resultados.
    - Adicionado gráfico de distribuição de assinaturas, permitindo visualizar a quantidade de usuários por tipo de plano (Free, Premium e Family) de acordo com o readme.

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 st.set_page_config(page_title="Dashboard Spotify", layout="wide")
 st.title("🎧 Dashboard Spotify")
@@ -7,7 +8,7 @@ st.title("🎧 Dashboard Spotify")
 # =========================
 # 1) Carregar base tratada
 # =========================
-df = pd.read_csv("data/spotify_tratado.csv")
+df = pd.read_csv("../data/spotify_tratado.csv")
 
 # =========================
 # 2) Garantir tipos (robusto)
@@ -20,8 +21,12 @@ df["inactive_3_months_flag"] = df["inactive_3_months_flag"].astype(str).str.lowe
 )
 
 # Anúncios (podem estar como 0/1)
+#for c in ["ad_interaction", "ad_conversion_to_subscription"]:
+    #df[c] = df[c].astype(str).str.lower().map({"1": True, "0": False, "true": True, "false": False})
+    # O erro na porcentagem de conversão está nessa conversão. A conversão dever ser de string para booleano.
+
 for c in ["ad_interaction", "ad_conversion_to_subscription"]:
-    df[c] = df[c].astype(str).str.lower().map({"1": True, "0": False, "true": True, "false": False})
+    df[c] = (df[c].astype(str).str.lower().map({"yes": True,"no": False}))    
 
 # Numéricos
 num_cols = [
@@ -68,10 +73,9 @@ avg_skips = df_filtrado["avg_skips_per_day"].mean()
 avg_rating = df_filtrado["music_suggestion_rating_1_to_5"].mean()
 
 # Conversão de anúncios (entre quem interagiu, quantos converteram)
-interagiram = df_filtrado["ad_interaction"].sum() if df["ad_interaction"].notna().any() else 0
-converteram = df_filtrado["ad_conversion_to_subscription"].sum() if df["ad_conversion_to_subscription"].notna().any() else 0
+interagiram = df_filtrado["ad_interaction"].sum() if df_filtrado["ad_interaction"].notna().any() else 0
+converteram = df_filtrado["ad_conversion_to_subscription"].sum() if df_filtrado["ad_conversion_to_subscription"].notna().any() else 0
 ad_conv_rate = (converteram / interagiram * 100) if interagiram else 0
-
 
 # Exibir KPIs
 c1, c2, c3, c4, c5, c6 = st.columns(6)
@@ -82,6 +86,32 @@ c4.metric("🎧 Horas/sem (média)", f"{avg_hours:.2f}")
 c5.metric("⏭️ Skips/dia (média)", f"{avg_skips:.2f}")
 c6.metric("📢 Conv. Anúncio (%)", f"{ad_conv_rate:.1f}%")
 
+# Graficos de distribuição de assinaturas
+st.divider()
+col1, col2 = st.columns(2)
+with col1:
+    st.subheader("📊 Quantidade de Usuários por Assinatura")
+    graph_bar_assinaturas = df["subscription_type"].value_counts()
+    st.bar_chart(graph_bar_assinaturas)
+
+with col2:
+    st.subheader("📊 Distribuição Percentual de Assinaturas")
+    graph_pie_assinatura = df["subscription_type"].value_counts().reset_index()
+    graph_pie_assinatura.columns = ["Tipo de Assinatura", "Quantidade"]
+
+    fig = px.pie(
+    graph_pie_assinatura,
+    names="Tipo de Assinatura",
+    values="Quantidade"
+    )
+
+    fig.update_layout(
+    font=dict(size=16)
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
+
+# Graficos de distribuição de assinaturas
 st.divider()
 st.subheader("📊Distribuição por Idade")
 grafico_idade = df_filtrado.groupby("age")["avg_listening_hours_per_week"].mean()
